3b4b85a51976b2823cf486f4f7f7598d11513bcc
Pablo <practica3@drakharstudio.com>
2022-11-11 13:05:30 +0000

    whatttt

--- Assets/Scripts/ScaleObject.cs
+++ Assets/Scripts/ScaleObject.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 
 public class ScaleObject : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
+    public Material hitMaterial;
     void Update()
     {
-        
+        //ccon tablet y con ordenador (0, pq es el bot izquu del ratón, 1 rueda o 2 el bot derech) (TouchPhase.Began/GetMouseButtonDown)
+        if ((Input.touchCount >= 1 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetMouseButtonUp(0)))
+        {
+            Vector3 pos = Input.mousePosition;
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                pos = Input.GetTouch(0).position;
+            }
+
+            Ray rayo = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayo, out hitInfo) == true)
+            {
+                if (hitInfo.collider.tag.Equals("Diana"))
+                {
+                    GameObject diana = hitInfo.collider.gameObject;
+                    hitInfo.collider.GetComponent<MeshRenderer>().material = hitMaterial;
+                    LeanTween.scale(diana, Vector3.one * 4f, 0.5f);
+
+                }
+
+            }
+            
+        }
     }
+
 }
